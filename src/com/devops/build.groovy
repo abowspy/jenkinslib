4b7e88f2c0688d0e88986afe6ab52afeca3250e8
陳包包 <abowspy@gmail.com>
2022-10-24 22:22:26 +0800

    Create build.groovy

---
+++ src/com/devops/build.groovy
@@ -0,0 +1,12 @@
+package com.devops
+
+def GoBuild(buildType, buildArgs) {
+
+    def buildTools = ["mvn":"M2", "ant":"ANT", "gradle":"GRADLE"]
+
+    println("選擇建構類型為 ${buildType}，參數為 ${buildArgs}")
+
+    BuildHome = tool buildTools[buildType]
+    sh "${BuildHome}/bin/buildType ${buildArgs}"
+
+}
